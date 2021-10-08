3f9e5e48629a508e12b05fcd522e055a3c68e66d
Abdu Yongho <yonghoabdurahim@gmail.com>
2021-10-08 11:48:15 -0500

    added initial commit

---
+++ src/main/java/com/yonghocompany/first_yongho_project/App.java
@@ -0,0 +1,13 @@
+package com.yonghocompany.first_yongho_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
